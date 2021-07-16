d81ea8043b556ebdd5d712feb03b3e37810e3f83
CaptainAwesome3771 <zuckerberg3771@gmail.com>
2021-07-16 15:20:19 +0500

    commi

---
+++ vis/adminstration_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdminstrationSystemConfig(AppConfig):
+    name = 'adminstration_system'
---
+++ vis/vis/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    context={}
+    template=loader.get_template('dashboard/index.html')
+    return HttpResponse(template.render(context,request))
+
+def other_html(request):
+    context={}
+    load_template='app/'+request.path.split('/')[-1]
+    # print("temp:"+load_template)
+    # return HttpResponse(load_template)
+    template=loader.get_template(load_template)
+
+    return HttpResponse(template.render(context,request))
