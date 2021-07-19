2b0d73be4fb02473b67fbe7c51eab41b43a2d815
CaptainAwesome3771 <zuckerberg3771@gmail.com>
2021-07-19 11:44:17 +0500

    commit

--- vis/assets/js/mine.js
+++ vis/assets/js/mine.js
@@ -1,24 +1,31 @@
-$(document).ready(function () {
-        
-        // Setup - add a text input to each footer cell
-       
-       
-        // DataTable
-        var table = $('#example').DataTable({
-            columnDefs: [{
-               targets: 0,
-                checkboxes:{
-                selectRow:true
-                },
-                className:'hello'
-            }],
-            select: {
-                style: 'multi',
-               
-            },
-            order: [[1, 'asc']]
-        });
-
-
-
+  // DataTable
+    var table = $('#example').DataTable({
+         
+    "columnDefs" : [
+        { targets: 0, sortable: false},
+    ],
+    "order": [[ 1, "asc" ]],
+    
     });
+
+ $('#checkall').click(function() {
+  if ($(this).is(':checked')) {
+    var checkboxes=document.querySelectorAll(".checkone")
+
+    for(var i=0;i<checkboxes.length;i++)
+    {
+        checkboxes[i].checked =true
+    }
+  }
+  else
+  {
+    var checkboxes=document.querySelectorAll(".checkone")
+    for(var i=0;i<checkboxes.length;i++)
+    {
+        checkboxes[i].checked =false
+    }
+  } 
+});
+
+var need_remove=document.querySelectorAll("#remove_class")
+need_remove[0].removeClass('sorting_asc')
